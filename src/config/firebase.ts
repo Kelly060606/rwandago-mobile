a8ea58a07a94e0ed0c7a3b0b3fcefe57e727b894
Ruzindaza <kezaruzindazaornellaa@gmail.com>
2026-08-31 00:37:35 +0200

    Update .gitignore and use environment variables for Firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,17 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCQMIv4WwDX3DK3rh9-vs3KyW3Zs16SpqA",
-  authDomain: "rwandago-bfa60.firebaseapp.com",
-  projectId: "rwandago-bfa60",
-  storageBucket: "rwandago-bfa60.firebasestorage.app",
-  messagingSenderId: "286592106499",
-  appId: "1:286592106499:web:4b0952ae525bf31c2ece36",
-  measurementId: "G-GLK4V31T93"
+  apiKey: "AIzaSyBhbH29TvPgi0vlxrSoltaDphIf38MqHJM",
+  authDomain: "rwandago-2.firebaseapp.com",
+  projectId: "rwandago-2",
+  storageBucket: "rwandago-2.firebasestorage.app",
+  messagingSenderId: "282798762645",
+  appId: "1:282798762645:web:c9dd5aafbe0b765fb2899c",
+  measurementId: "G-D37HTRNVW2"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-console.log('Firebase initialized successfully!');
